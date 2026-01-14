ccbc5286ce4f6ebe14b17a6284ebab35e7f18fb9
Elivius <elivius@elivius.de>
2026-01-14 16:52:37 +0800

    feat: add wallet_agent

--- ai-agents/mindy/sub_agents/__init__.py
+++ ai-agents/mindy/sub_agents/__init__.py
@@ -3,3 +3,4 @@ from .farewell_agent import farewell_agent
 from .security_agent import security_agent
 from .yield_agent import yield_agent
 from .parser_agent import parser_agent
+from .wallet_agent import wallet_agent
