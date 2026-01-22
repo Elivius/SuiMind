49298af024642425c48c03cca52bc0bd31a25452
Elivius <elivius@elivius.de>
2026-01-23 00:18:13 +0800

    chore: remove comment

--- frontend/lib/suiClient.ts
+++ frontend/lib/suiClient.ts
@@ -1,6 +1,5 @@
 import { SuiGraphQLClient } from '@mysten/sui/graphql';
 
-// You create it ONCE here
 export const gqlClient = new SuiGraphQLClient({
   url: process.env.NEXT_PUBLIC_GQL_URL as string,
 });
