3ee425e1ab3c02bacf6747a017d87cf88c3eca4d
Elivius <elivius@elivius.de>
2026-01-22 23:49:08 +0800

    feat: add getTransaction | add reuse function/logic to utils

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -9,3 +9,7 @@ export const navigation = [
 ]
 
 export const SUI_COIN_TYPE = "0x2::sui::SUI";
+export const MIST_PER_SUI = 1_000_000_000;
+
+// For graphQL
+export const GQL_SUI_COIN_TYPE = "0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI"
