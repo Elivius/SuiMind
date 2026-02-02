1224da62b2505988417d96644456f57b6115d005
Yoo Jun Yang <yoojunyang0323@gmail.com>
2026-02-02 22:58:27 +0800

    feat: add send and request function

---
+++ frontend/hooks/usePaymentRequests.ts
@@ -0,0 +1,38 @@
+import { useSuiClientQuery, useCurrentAccount } from "@mysten/dapp-kit";
+
+export function usePaymentRequests() {
+    const account = useCurrentAccount();
+
+    const { data: ownedObjects, refetch } = useSuiClientQuery('getOwnedObjects', {
+    owner: account?.address || '',
+    filter: {
+      StructType: "0x5ae2ee3de630c587707ae71729e54e272cbab874a465ade2939ae8cf71d4c26d::request::PaymentRequest",
+    },
+    options: { showContent: true }
+    }, {
+        enabled: !!account?.address,
+        gcTime: 0,       
+        staleTime: 0,     
+        refetchOnMount: true,
+    });
+
+    const pendingRequests = ownedObjects?.data?.map((obj: any) => {
+        const fields = obj.data?.content?.fields;
+        return {
+        id: obj.data?.objectId,
+        requester: fields.requester,
+        recipient: fields.recipient,
+        amountMist: fields.amount,
+        amountSui: Number(fields.amount) / 1_000_000_000,
+        requestCode: fields.request_code,
+        };
+    }).filter(req => req.recipient === account?.address)  || [];
+
+    
+
+    return {
+        pendingRequests,
+        hasUnread: pendingRequests.length > 0,
+        refetch
+    };
+}
